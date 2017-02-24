e79b9bd3bf915633e5bae5979a60cd0d51f1ca06
Benjamin Horn <hornbd96@gmail.com>
2017-02-24 01:48:04 -0500

    Added binary search tree

---
+++ cmake-build-debug/CMakeFiles/datastructs.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/datastructs.dir/main.cpp.o"
+  "datastructs.pdb"
+  "datastructs"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/datastructs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ test/bst.h
@@ -0,0 +1,15 @@
+//
+// Created by Benjamin D. Horn on 2/24/17.
+//
+
+#ifndef DATASTRUCTS_BST_H
+#define DATASTRUCTS_BST_H
+
+
+class bst {
+
+
+};
+
+
+#endif //DATASTRUCTS_BST_H
